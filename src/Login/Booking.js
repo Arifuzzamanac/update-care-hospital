751e2625e6cd0c22fc9c224b9c679897afff8cea
Arifuzzamanac <arifac653@gmail.com>
2021-11-26 20:48:48 +0600

    first commit

--- src/Login/Booking.js
+++ src/Login/Booking.js
@@ -1,18 +1,33 @@
-import React from 'react';
-import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
+// import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
 const Booking = () => {
     const { ServiceId } = useParams();
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('/services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
+    const singledata = services.find(service => service?.id == ServiceId)
+    console.log(singledata)
+
     // const { name } = props.service;
     return (
         <div>
-            <h2>This is booking {ServiceId}</h2>
-            {/* <h1>Name: {name}</h1> */}
+            <div>
+                {/* <h2>This is booking {ServiceId}</h2> */}
+                {/* <h1>Name: {name}</h1> */}
+                <img style={{ height: '300px', width: '300px' }} src={singledata?.img} alt="" />
+                <h1>{singledata?.name}</h1>
+                <p>{singledata?.description}</p>
 
-            <Link to='/'>
-                <button className='btn btn-primary'>Go Home</button>
-            </Link>
+                <Link to='/'>
+                    <button className='btn btn-primary'>Go Home</button>
+                </Link>
+            </div>
         </div>
     );
 };
